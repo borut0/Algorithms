58ae9f7fe56b15ce188eac05288f74132920f68e
borut0 <89539128+borut0@users.noreply.github.com>
2021-08-25 23:15:40 +0530

    Update factrecursion.cpp

--- factorial/using recursion/factrecursion.cpp
+++ factorial/using recursion/factrecursion.cpp
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+#include<conio.h>
+int fact(int n){
+     if(n>0){
+	return n*fact(n-1);
+     }
+     else{
+      return 1;
+     }
+}
+void main(){
+	int no;
+	clrscr();
+	printf("Enter no:");
+	scanf("%d",&no);
+	if(no>=0){
+		printf("factorial is %d",fact(no));
+	}
+	else{
+		printf("wrong input");
+	}
+	getch();
+}
